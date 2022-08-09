1d450e1f97c202ba950347c6eb32c5950ea0dc42
Mortuus <mortuus.testing@gmail.com>
2022-08-09 16:55:30 +0700

    Update API configuration

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,14 @@
     },
     methods: {
       async getSpecificData(id) {
-        const res = await fetch(`http://localhost:3000/tasks/${id}`)
+        const res = await fetch(`https://mortuus-json-fake-server.herokuapp.com/tasks/${id}`)
         const data = await res.json()
         return data
       },
       async onFavorite(id) {
         const data = await this.getSpecificData(id)
         const updated_data = await {...data, favorite : !data.favorite}
-        const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+        const res = await fetch(`https://mortuus-json-fake-server.herokuapp.com/tasks/${id}`, {
           method: 'PUT',
           headers: {
             'Content-Type' : 'application/json'
@@ -39,7 +39,7 @@
       async onFinish(id) {
         const data = await this.getSpecificData(id)
         const updated_data = await {...data, finish : !data.finish}
-        const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+        const res = await fetch(`https://mortuus-json-fake-server.herokuapp.com/tasks/${id}`, {
           method: 'PUT',
           headers: {
             'Content-Type' : 'application/json'
@@ -49,14 +49,14 @@
       },
       async onDelete(id) {
         if (confirm("Are you sure?")) {
-          const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+          const res = await fetch(`https://mortuus-json-fake-server.herokuapp.com/tasks/${id}`, {
             method: 'DELETE'
           })
           this.tasks = this.tasks.filter((task) => task.id !== id)   
         }
       },
       async onSubmit(task) {
-        const res = await fetch("http://localhost:3000/tasks", {
+        const res = await fetch("https://mortuus-json-fake-server.herokuapp.com/tasks", {
           method: 'POST',
           headers: {'Content-Type' : 'application/json'},
           body: JSON.stringify(task)
@@ -66,7 +66,7 @@
       }
     },
     async created() {
-      const res = await fetch("http://localhost:3000/tasks")
+      const res = await fetch("https://mortuus-json-fake-server.herokuapp.com/tasks")
       const data = await res.json()
       this.tasks = await data
     }
